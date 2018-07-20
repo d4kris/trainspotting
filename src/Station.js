856e9ebc72fb42e8b379542b1d12823269cf273c
d4kris <kristin@liberger.com>
2018-07-20 11:06:24 +0200

    Nicer picker header

--- src/Station.js
+++ src/Station.js
@@ -1,25 +1,5 @@
 import React, {Component} from 'react';
 
-const Stations = (props) => {
-  if (!props.showPicker) {
-    return <span onClick={props.togglePicker}>Change</span>
-  }
-  return (
-    <select className="stations-list"
-            value={props.selected} onChange={props.selectStation}>
-      {props.list.map(item => {
-        return (
-          <option key={item.id} value={item.id}>
-            {item.name}
-          </option>
-        );
-      })}
-    </select>
-  );
-};
-
-
-
 class Station extends Component {
   state = {
     showPicker: this.props.showPicker,
@@ -34,26 +14,32 @@ class Station extends Component {
 
   togglePicker = () => {
     this.setState({ showPicker: !this.state.showPicker });
-  }
+  };
 
   selectStation = (e) => {
     this.setState({
       selected: e.target.value,
       showPicker: false
     })
-  }
+  };
+
   render() {
+    if (!this.state.showPicker) {
+      return <span onClick={this.togglePicker}>{this.stationName()}</span>
+    }
     return (
-      <span>
-        <span onClick={this.togglePicker}>{this.stationName()}</span>
-        <Stations list={this.props.stations}
-          showPicker={this.state.showPicker}
-          togglePicker={this.togglePicker}
-          selectStation={this.selectStation}
-          selected={this.state.selected}
-        />
-      </span>
-    )
+      <select className="stations-list"
+              value={this.state.selected}
+              onChange={this.selectStation}>
+        {this.props.stations.map(item => {
+          return (
+            <option key={item.id} value={item.id}>
+              {item.name}
+            </option>
+          );
+        })}
+      </select>
+    );
   }
 }
 
